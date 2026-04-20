937a1400dc77f9a18697de6df8a9b02b009cec0e
nieuwsvandaag21 <daxprogamernl@gmail.com>
2026-04-20 04:19:06 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -4,29 +4,25 @@ print `cd .. && pwd`.strip
 print "! Press Enter To Continue."
 gets
 system "sudo pacman -S base-devel git xorg-server xorg-xinit libx11 libxinerama libxft --noconfirm"
-
 Dir.chdir("..")
 system "git clone https://git.suckless.org/dwm"
 system "git clone https://git.suckless.org/st"
-
 Dir.chdir("st")
 system "sudo make clean install"
-
 Dir.chdir("../dwm")
 system "sudo make clean install"
-
 system "sudo mkdir -p /usr/local/share/fonts/"
 system "cp /Dwm-Script/JetBrainsMonoNerdFont-Regular.ttf /usr/local/share/fonts/"
 system "fc-cache -fv"
-
 st_path = `which st`.strip
-
 config_path = "config.h"
 content = File.read(config_path)
-
 updated = content.gsub(
   %r{#define SHCMD\(cmd\) \{ \.v = \(const char\*\[\]\)\{ "/bin/sh", "-c", cmd, NULL \} \}},
   "#define SHCMD(cmd) { .v = (const char*[]){ \"#{st_path}\", \"-c\", cmd, NULL } }"
 )
-
+updated = updated.gsub(
+  /static const char \*fonts\[\]\s*=\s*\{[^}]*\};/,
+  'static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=26" };'
+)
 File.write(config_path, updated)
