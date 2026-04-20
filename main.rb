47ede5212eaf7a273e55d5f8d2d7bd65e46ac788
nieuwsvandaag21 <daxprogamernl@gmail.com>
2026-04-20 03:24:31 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 system "clear"
 puts "Make Sure To Have Updated Your Linux System Before Running This Program And To Run It One Directory Below (The Dwm-Script Directory Is The One Below) Where You Want Your Dwm And St Directories Confirm This Is The Directory You Want Your Dwm And St Directories In"
 system ("cd .. && pwd")
-print("!\nPress Enter To Continue.")
+print "!\nPress Enter To Continue."
 gets
 system "sudo pacman -S base-devel git xorg-server xorg-xinit libx11 libxinerama libxft --noconfirm"
 system "cd .."
