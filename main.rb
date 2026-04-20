98d0d8f9a786e6f3eced83810e44c8adaa7f147d
nieuwsvandaag21 <daxprogamernl@gmail.com>
2026-04-20 03:22:35 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
 system "clear"
-puts "Make Sure To Have Updated Your Linux System Before Running This Program And To Run It One Directory Below (The Dwm-Script Directory Is The One Below) Where You Want Your Dwm And St Directories Make Sure Of This By Using \"pwd\" The Output Should Show Your Current Directory!\nPress Enter To Continue."
+puts "Make Sure To Have Updated Your Linux System Before Running This Program And To Run It One Directory Below (The Dwm-Script Directory Is The One Below) Where You Want Your Dwm And St Directories Confirm This Is The Directory You Want Your Dwm And St Directories In"
+system ("cd .. && pwd")
+print("!\nPress Enter To Continue.")
 gets
 system "sudo pacman -S base-devel git xorg-server xorg-xinit libx11 libxinerama libxft --noconfirm"
 system "cd .."
@@ -9,3 +11,6 @@ system "cd st"
 system "sudo make clean install"
 system "cd ../dwm"
 system "sudo make clean install"
+
+
+\nPress Enter To Continue."
