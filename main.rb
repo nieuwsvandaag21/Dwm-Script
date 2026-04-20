1648a2a8e06beb2da4f7a1429ff4ef50246188da
nieuwsvandaag21 <daxprogamernl@gmail.com>
2026-04-20 02:51:03 +0200

    Initial Commit

---
+++ main.rb
@@ -0,0 +1,6 @@
+system "clear"
+puts "Make Sure To Have Updated Your Linux System Before Running This Program!\nPress Enter To Continue."
+gets
+system "sudo pacman -S base-devel git"
+system "clear"
+
