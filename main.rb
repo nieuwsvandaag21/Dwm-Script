f9047cde174585a375dc1c43efeac6fa953ac4a2
nieuwsvandaag21 <daxprogamernl@gmail.com>
2026-04-20 22:30:41 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -21,13 +21,17 @@ system "sudo make clean install"
 Dir.chdir("../dwm")
 system "sudo make clean install"
 
+Dir.chdir("..")
+current_user = `whoami`.strip
+system "sudo chown -R #{current_user}:#{current_user} dwm st"
+
 system "sudo mkdir -p /usr/local/share/fonts/"
-system "cp /Dwm-Script/JetBrainsMonoNerdFont-Regular.ttf /usr/local/share/fonts/"
+system "cp ~/Dwm-Script/JetBrainsMonoNerdFont-Regular.ttf /usr/local/share/fonts/"
 system "fc-cache -fv"
 
+Dir.chdir("/dwm")
 st_path     = `which st`.strip
 config_path = "config.h"
-system "sudo chmod 644 config.h"
 content     = File.read(config_path)
 
 updated = content.gsub(
