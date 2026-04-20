ea6bc1a958d791e0542afebc0223a9001e8132af
nieuwsvandaag21 <daxprogamernl@gmail.com>
2026-04-20 04:40:20 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,30 +1,44 @@
 system "clear"
-print "Make Sure To Have Updated Your Linux System Before Running This Program And To Run It One Directory Below (The Dwm-Script Directory Is The One Below) Where You Want Your Dwm And St Directories Confirm This Is The Directory You Want Your Dwm And St Directories In "
+
+print "Make Sure To Have Updated Your Linux System Before Running This Program " \
+      "And To Run It One Directory Below (The Dwm-Script Directory Is The One Below) " \
+      "Where You Want Your Dwm And St Directories " \
+      "Confirm This Is The Directory You Want Your Dwm And St Directories In "
 print `cd .. && pwd`.strip
 print "! Press Enter To Continue."
 gets
+
 system "sudo pacman -S base-devel git xorg-server xorg-xinit libx11 libxinerama libxft --noconfirm"
+
 Dir.chdir("..")
+
 system "git clone https://git.suckless.org/dwm"
 system "git clone https://git.suckless.org/st"
+
 Dir.chdir("st")
 system "sudo make clean install"
+
 Dir.chdir("../dwm")
 system "sudo make clean install"
+
 system "sudo mkdir -p /usr/local/share/fonts/"
 system "cp /Dwm-Script/JetBrainsMonoNerdFont-Regular.ttf /usr/local/share/fonts/"
 system "fc-cache -fv"
-st_path = `which st`.strip
+
+st_path     = `which st`.strip
 config_path = "config.h"
 system "chmod 644 config.h"
-content = File.read(config_path)
+content     = File.read(config_path)
+
 updated = content.gsub(
-  %r{#define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }},
-  "#define SHCMD(cmd) { .v = (const char*[]){ "#{st_path}", "-c", cmd, NULL } }"
+  %r{#define SHCMD\(cmd\) \{ \.v = \(const char\*\[\]\)\{ "/bin/sh", "-c", cmd, NULL \} \}},
+  "#define SHCMD(cmd) { .v = (const char*[]){ \"#{st_path}\", \"-c\", cmd, NULL } }"
 )
+
 updated = updated.gsub(
-  /static const char fonts[]\s=\s*{[^}]*};/,
+  /static const char fonts\[\]\s*=\s*\{[^}]*\};/,
   'static const char *fonts[] = { "JetBrainsMono Nerd Font:size=26" };'
 )
+
 File.write(config_path, updated)
 File.write(File.expand_path("~/.xinitrc"), "exec dwm\n")
